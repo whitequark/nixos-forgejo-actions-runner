2bbd9a58a334fb0df7aa94ad32f1b77a1be31261
Catherine <whitequark@whitequark.org>
2026-03-04 22:52:49 +0000

    Add limited support for bare-metal hardware.

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -1,4 +1,4 @@
-{ modulesPath, lib, ... }:
+{ modulesPath, lib, serverName, siteConfig, ... }:
 
 with lib;
 
@@ -7,40 +7,69 @@ recursiveUpdate {
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        # USB
+        "uhci_hcd"
+        "ehci_pci"
+        "usb_storage"
+        "usbhid"
+        # (S)ATA
+        "ata_piix"
+        "ahci"
+        "sd_mod"
+        # NVMe
+        "nvme"
+        # Xen
+        "xen_blkfront"
+      ];
+    };
+
+    loader.timeout = 15;
+    loader.grub.memtest86.enable = true;
+
+    tmp.cleanOnBoot = true;
+  };
+
+  hardware = {
+    enableRedistributableFirmware = true;
+
+    cpu.intel.updateMicrocode =
+      (if siteConfig.host ? cpuVendor then siteConfig.host.cpuVendor == "intel" else false);
+    cpu.amd.updateMicrocode =
+      (if siteConfig.host ? cpuVendor then siteConfig.host.cpuVendor == "amd" else false);
+  };
+
   fileSystems."/" = {
     device = "/dev/disk/by-label/root";
     fsType = "ext4";
   };
 
-  boot = {
-    initrd = {
-      availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
-      kernelModules = [ "nvme" ];
-    };
-
-    loader.timeout = 15;
-    tmp.cleanOnBoot = true;
-  };
-
   swapDevices = (optional (pathExists "/swap") {
     device = "/swap";
     size = 2048;
+  }) ++ (optional (pathExists "/dev/disk/by-label/swap") {
+    device = "/dev/disk/by-label/swap";
   });
   zramSwap.enable = true;
 
 } (if (pathExists /sys/firmware/efi) then {
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
-  };
-
+  # UEFI system
   boot.loader.grub = {
     efiSupport = true;
     efiInstallAsRemovable = true;
     device = "nodev";
   };
 
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
+
 } else {
+  # BIOS system
   boot.loader.grub.device = (
     head (filter pathExists ["/dev/sda" "/dev/vda"])
   );
