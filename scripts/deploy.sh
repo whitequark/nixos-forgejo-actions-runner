66c47c89d688d03b3cb03edfb7e0974cbba5bc22
Catherine <whitequark@whitequark.org>
2026-05-14 18:11:17 +0000

    Update deploy script to allow specifying IP directly.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+set -e
 
 HOST=$1
+IP=$2
 if [ -z "${HOST}" ]; then
-    echo "Usage: $0 hostname"
+    echo "Usage: $0 hostname [ip]"
     exit 1
 fi
 
-rsync -vrt --delete-excluded -f "+ site/${HOST}.toml" -f "- site/*" nixos/ root@${HOST}:/etc/nixos
-ssh root@${HOST} "HOST=${HOST} nixos-rebuild switch -f /etc/nixos/pivot.nix --no-reexec --show-trace"
+rsync -vrt --delete-excluded -f "+ site/${HOST}.toml" -f "- site/*" nixos/ root@${IP:-HOST}:/etc/nixos
+ssh root@${IP:-HOST} "HOST=${HOST} nixos-rebuild switch -f /etc/nixos/pivot.nix --no-reexec --show-trace"
